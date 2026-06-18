cb03b188d6beb192e2b9db78331dec9a5a859ddd
Andrey Ivanov <this.andrey@gmail.com>
2026-06-19 00:50:57 +0300

    fix warnings

--- packages/pro_lsp/test/e2e_workspace_test.dart
+++ packages/pro_lsp/test/e2e_workspace_test.dart
@@ -59,7 +59,7 @@ void main() {
           server.workspace.onDiagnostic(
             (params, context) async => WorkspaceDiagnosticReport(
               items: [
-                WorkspaceDocumentDiagnosticReport.workspaceFullDocumentDiagnosticReport(
+                .workspaceFullDocumentDiagnosticReport(
                   const WorkspaceFullDocumentDiagnosticReport(
                     items: [],
                     uri: 'file:///a.dart',
